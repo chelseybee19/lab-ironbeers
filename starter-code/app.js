4e29aaa5221ec107485a29a80a33e297b1ed2faa
Chelsey Grice <chelseygrice92@gmail.com>
2020-03-28 07:57:16 +0100

    commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', hbs));
+app.get('/', (req, res) => res.render('layout'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then(beer => res.render('beers', {beer}))
+    //   .catch(error => console.log(error)); not sure if I need to add it here
+  });
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+    .then(beer => res.render('random-beer', {beer}))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
